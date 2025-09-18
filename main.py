b2b1772c328cc03240ae829e92c62e3b3c6ed884
lijiabao1 <3533440766@qq.com>
2025-09-18 20:02:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-# 在这个文件里编写代码
+weight=float(input ("在地球上的体重为:"))
+a=0.5
+for year in range(1, 11):  
+    earth_weight = a*year+weight  
+    moon_weight = earth_weight * 0.165  
+    print(f"第{year}年，在地球上的体重：{earth_weight:.2f}kg，在月球上的体重：{moon_weight:.2f}kg")
